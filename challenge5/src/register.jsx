f468c30a44992fd4a961737cbc959bc722d85f00
RafiDhafinErsamazaya <160104105+RafiDhafinErsamazaya@users.noreply.github.com>
2024-04-19 18:54:20 +0700

    Update register.jsx

--- challenge5/src/register.jsx
+++ challenge5/src/register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
@@ -36,6 +36,14 @@ export default function Register() {
       alert("Registration failed. Please try again.");
     }
   };
+
+  useEffect(() => {
+    console.log("localStorage ", localStorage.getItem("token"));
+    if (localStorage.getItem("token") !== null) {
+      alert("Token masih aktif");
+      navigate("/");
+    }
+  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen">
